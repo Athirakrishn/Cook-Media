d3c2cffbb3921b910bd852281384bf94fb4b9716
Athirakrishn <athiraramakrishnan41020@gmail.com>
2025-11-19 23:43:10 +0530

    about created

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Footer } from "../footer/footer";
+import { Header } from "../header/header";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [Footer, Header,RouterLink],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
